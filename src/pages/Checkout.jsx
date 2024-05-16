0e08fc119871adff239fa7f90b0b6affbaf4d2b6
Eduardo Enrique Navarro Echeverria <eenavarro@sprc.com.co>
2024-05-16 08:49:48 -0500

    Responsive shopping cart

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { myContextCart } from "../context/context";
 import { useForm } from "react-hook-form";
+import toast from 'react-hot-toast';
+import ToastConfig from "../utils/ToastConfig.jsx";
 import "./Checkout.css";
 
 const Checkout = () => {
@@ -14,9 +16,11 @@ const Checkout = () => {
   };
 
   return (
+
     <div className="carrito-container">
+
       <div className="carrito-izquierda">
-        <h2>Carrito de Compras</h2>
+        <h2>Shopping Cart</h2>
         <ul className="productos-lista">
           {cart.map((product) => (
             <li key={product.id}>
@@ -27,90 +31,36 @@ const Checkout = () => {
             </li>
           ))}
         </ul>
-        <p>Total price: ${totalPrice()}</p>
+        <strong><p>Total price: ${totalPrice()}</p></strong>
         <div className="opciones-carrito">
           <button className="vaciar-carrito" onClick={cleanCart}>Clean cart</button>
         </div>
       </div>
 
       <div className="carrito-derecha">
-        <h2>Datos de Pago</h2>
+
+        <h2>Payment details</h2>
         <form className="datos-pago-form" onSubmit={handleSubmit(send)}>
           <label>Name:</label>
-          <input type="text" {...register("name")} />
+          <input type="text" {...register("name", { required: "required" })} />
 
           <label>Email:</label>
-          <input type="email" {...register("email")} />
+          <input type="email" {...register("email", { required: "required" })} />
 
           <label>Phone:</label>
-          <input type="tel" {...register("phone")} />
+          <input type="tel" {...register("phone", { required: "required" })} />
 
           <label>Addres:</label>
-          <input type="text" {...register("addres")} />
+          <input type="text" {...register("address", { required: "required" })} />
 
-          <button className="confirm-cart" type="submit">Confirm</button>
+          <button className="confirm-cart" type="submit" disabled={cart.length === 0}>Confirm</button>
         </form>
+
       </div>
+
     </div>
+
   );
 };
 
 export default Checkout;
-
-// const Clothes = () => {
-
-//   const {register, handleSubmit} = useForm();
-
-//   /*
-//     const [name, setName] = useState('');
-//     const [email, setEmail] = useState('');
-//     const [phone, setPhone] = useState('');
-//   */
-//   /*const handleSubmit = (e) => {
-//     e.preventDefault();
-
-//     console.log('Formulario enviado:',  formData );
-//   };
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData({ ...formData, [name]: value });
-//   };
-
-//     const handleName = (e) => {
-//       setName(e.target.value)
-//     }
-
-//     const handleEmail = (e) => {
-//       setEmail(e.target.value)
-//     }
-
-//     const handlePhone = (e) => {
-//       setPhone(e.target.value)
-//     }
-//   */
-
-//     const send = (data) => {
-//       console.log(data);
-//     }
-
-//   return (
-//     <form onSubmit={(handleSubmit(send))}>
-//       <div>
-//         <label>Nombre:</label>
-//         <input type="text" {...register('name')} />
-//       </div>
-//       <div>
-//         <label>Correo:</label>
-//         <input type="email" {...register('email')} />
-//       </div>
-//       <div>
-//         <label>Teléfono:</label>
-//         <input type="tel" {...register('phone')}  />
-//       </div>
-//       <button type="submit">Enviar</button>
-//     </form>
-//   );
-// };
-
-// export default Clothes;
